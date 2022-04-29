b8b6373d7cf7a42b330d35d3937cf1f5c9114900
yaayaya <44697709+yaayaya@users.noreply.github.com>
2022-04-29 20:46:01 +0800

    0429 2045

---
+++ dist/bak/test_i18/lang.js
@@ -0,0 +1,25 @@
+let zhtw = {
+  translation: {
+    input: {
+      placeholder: "a placeholder"
+    },
+    nav: {
+      home: 'Home',
+      page1: 'Page One',
+      page2: 'Page Two'
+    }
+  }
+}
+
+let en = {
+  translation: {
+    input: {
+      placeholder: "a placasdfsavcsvseholder"
+    },
+    nav: {
+      home: 'Hocccsvcdsvme',
+      page1: 'Pageasdvsadv One',
+      page2: 'Pageasdvsadc Two'
+    }
+  }
+}
