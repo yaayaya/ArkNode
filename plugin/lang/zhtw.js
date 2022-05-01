c48ea0ff0d012a586c6d9e9a422361ae1a13b0c7
yaayaya <44697709+yaayaya@users.noreply.github.com>
2022-05-01 15:34:38 +0800

    0501

--- plugin/lang/zhtw.js
+++ plugin/lang/zhtw.js
@@ -16,6 +16,11 @@ let zhtw = {
       Whale: "鯨魚傳說",
       OtherRuins: "其他遺址",
       Store: "歡迎來到方舟博物館商店",
+
+      giant: "巨人巨獸化石",
+      biological: "生物化石",
+      plant: "植物化石",
+      BuNam: "卜湳文明遺跡遺跡文物",
     },
     Lang: {
       Title: "語言",
@@ -148,45 +153,199 @@ let zhtw = {
       t5: "",
       t6: "",
     },
-    BuNam_1:{
-        Title : "「卜湳文明遺跡-大崎遺址」在台灣台南",
-        t1: '這個遺跡於西元2001-2003年時發現一個窖藏遺跡一個墓葬遺址，位於臺南縣官田鄉大崎村臺南藝術大學校地内。在進行小規模的8個探方挖掘後，發現了疑是石壁結構的部分遺跡，於是延著該線索找出此墓室的全區範圍。',
-        t2: '此墓葬為石室結構・南北長900釐米・東西寬450釐米・四面均有淺浮雕・西面牆百分之八十倒塌・石室應為負斗形頂・均已倒塌中央有一石棺長198釐米·寬105釐米，高50釐米·第一期的挖掘工作發現石室内有大量的石刻浮雕或石刻造像與陶俑。',
-        t3: '後來陸續出土「十二聖獸」、「魂遁之門」與「石塔」等文物。2001年與2003年該出土文物展覽時，造成轟動，引發熱烈關注。卜湳文明遺跡為台灣所發現的重要考古遺跡，不僅具有考古學及歷史學方面的價值，還涉及政治、宗教、民族、神話、藝術等領域研究的開創性，再次證明了台灣文化的起源是多元一體的。',
-        t4: '',
-        t5: '',
-        t6: '',
+    BuNam_1: {
+      Title: "「卜湳文明遺跡-大崎遺址」在台灣台南",
+      t1: "這個遺跡於西元2001-2003年時發現一個窖藏遺跡一個墓葬遺址，位於臺南縣官田鄉大崎村臺南藝術大學校地内。在進行小規模的8個探方挖掘後，發現了疑是石壁結構的部分遺跡，於是延著該線索找出此墓室的全區範圍。",
+      t2: "此墓葬為石室結構，南北長900釐米，東西寬450釐米，四面均有淺浮雕，西面牆百分之八十倒塌，石室應為負斗形頂，均已倒塌中央有一石棺長198釐米·寬105釐米，高50釐米·第一期的挖掘工作發現石室内有大量的石刻浮雕或石刻造像與陶俑。",
+      t3: "後來陸續出土「十二聖獸」、「魂遁之門」與「石塔」等文物。2001年與2003年該出土文物展覽時，造成轟動，引發熱烈關注。卜湳文明遺跡為台灣所發現的重要考古遺跡，不僅具有考古學及歷史學方面的價值，還涉及政治、宗教、民族、神話、藝術等領域研究的開創性，再次證明了台灣文化的起源是多元一體的。",
+      t4: "",
+      t5: "",
+      t6: "",
     },
-    BuNam_2:{
-        Title : "「卜湳文明魂遁之輪遺址」在英國古德伍德莊園",
-        t1: '這個遺跡於2016年發現於英國西薩塞克斯郡的Goodwood古德伍德莊園・占地26英畝。Goodwood古德德莊園是由Wilfred和Jeannette Cass夫婦在22年前創立的英國卡斯雕塑基金會的雕塑公園。',
-        t2: '「卜湳文明魂遁之輪遺址」在5個4公尺見方，最深約2公尺的探方坑，發現了雨個巨大圓盤還有其他散置的不明金屬物質，共約有25個，這些不同造型的神秘巨型金屬塊相當完整，看似能夠組装的造形，很多物件都有雨個相對的形状，上面有著神秘的浮雕與文字印記。',
-        t3: '少數地方銹蝕丶殘缺、破損，表面呈現類以銅綠顏色，並夾雜著當地的白堊土·金屬造型與颜色均相當吸引人。這究竟由何人製作？如何製作？作為何用？組装後的造形和功能究竟是什麼？這些神秘的浮雕透露著什麼訊息？都有待相關學者繼續思考與研究。',
-        t4: '在歷經6年的挖掘和研究，這些散置在遺址坑內的不明金屬物件被運回台灣，並且組裝起來。在高雄中央公園展出時，引起相當多的討論和關注。2022年6月轉往台北空總當代文化實驗場展出。',
-        
-        t5: '2017年在英國古德伍德農場發現了3塊神秘的巨型金屬板，這些金屬板上有著神秘的浮雕印記，浮雕大部分扭曲皺褶變形，這究竟由何人製作？如何製作？作為何用？不知是否因埋藏於地下的墓穴崩塌或地層變化造成，這些神秘的浮雕透露著什麼訊息？這些金屬的扭曲擠壓是否意謂著地球已經發生劇烈的地層變動？',
-        t6: '金屬板上除了皺褶的印記浮雕外，表面有的呈現熔岩狀，有的地方銹蝕、殘缺、破損，但邊緣被銳利切割，呈現碑狀。',
-        t7: '這些埋藏在地底下的巨型金屬板被誰所切割的？以什麼機具切割的？都有待我們繼續思考與研究。',
-        t8: '這3塊神秘的巨型金屬板，也在2017年到英國Victoria and Albert Museum 的黎明畫廊展出，獲得極大的迴響。'
+    BuNam_2: {
+      Title: "「卜湳文明魂遁之輪遺址」在英國古德伍德莊園",
+      t1: "這個遺跡於2016年發現於英國西薩塞克斯郡的Goodwood古德伍德莊園，占地26英畝。Goodwood古德德莊園是由Wilfred和Jeannette Cass夫婦在22年前創立的英國卡斯雕塑基金會的雕塑公園。",
+      t2: "「卜湳文明魂遁之輪遺址」在5個4公尺見方，最深約2公尺的探方坑，發現了雨個巨大圓盤還有其他散置的不明金屬物質，共約有25個，這些不同造型的神秘巨型金屬塊相當完整，看似能夠組装的造形，很多物件都有雨個相對的形状，上面有著神秘的浮雕與文字印記。",
+      t3: "少數地方銹蝕丶殘缺、破損，表面呈現類以銅綠顏色，並夾雜著當地的白堊土·金屬造型與颜色均相當吸引人。這究竟由何人製作？如何製作？作為何用？組装後的造形和功能究竟是什麼？這些神秘的浮雕透露著什麼訊息？都有待相關學者繼續思考與研究。",
+      t4: "在歷經6年的挖掘和研究，這些散置在遺址坑內的不明金屬物件被運回台灣，並且組裝起來。在高雄中央公園展出時，引起相當多的討論和關注。2022年6月轉往台北空總當代文化實驗場展出。",
+
+      t5: "2017年在英國古德伍德農場發現了3塊神秘的巨型金屬板，這些金屬板上有著神秘的浮雕印記，浮雕大部分扭曲皺褶變形，這究竟由何人製作？如何製作？作為何用？不知是否因埋藏於地下的墓穴崩塌或地層變化造成，這些神秘的浮雕透露著什麼訊息？這些金屬的扭曲擠壓是否意謂著地球已經發生劇烈的地層變動？",
+      t6: "金屬板上除了皺褶的印記浮雕外，表面有的呈現熔岩狀，有的地方銹蝕、殘缺、破損，但邊緣被銳利切割，呈現碑狀。",
+      t7: "這些埋藏在地底下的巨型金屬板被誰所切割的？以什麼機具切割的？都有待我們繼續思考與研究。",
+      t8: "這3塊神秘的巨型金屬板，也在2017年到英國Victoria and Albert Museum 的黎明畫廊展出，獲得極大的迴響。",
     },
-    BuNam_3:{
-        Title : "荷蘭國家博物館個展",
-        t1: '這個遺跡於2016年發現於英國西薩塞克斯郡的Goodwood古德伍德莊園・占地26英畝。Goodwood古德德莊園是由Wilfred和Jeannette Cass夫婦在22年前創立的英國卡斯雕塑基金會的雕塑公園。',
-        t2: '「卜湳文明魂遁之輪遺址」在5個4公尺見方，最深約2公尺的探方坑，發現了雨個巨大圓盤還有其他散置的不明金屬物質，共約有25個，這些不同造型的神秘巨型金屬塊相當完整，看似能夠組装的造形，很多物件都有雨個相對的形状，上面有著神秘的浮雕與文字印記。',
-        t3: '少數地方銹蝕丶殘缺、破損，表面呈現類以銅綠顏色，並夾雜著當地的白堊土·金屬造型與颜色均相當吸引人。這究竟由何人製作？如何製作？作為何用？組装後的造形和功能究竟是什麼？這些神秘的浮雕透露著什麼訊息？都有待相關學者繼續思考與研究。',
-        t4: '在歷經6年的挖掘和研究，這些散置在遺址坑內的不明金屬物件被運回台灣，並且組裝起來。在高雄中央公園展出時，引起相當多的討論和關注。2022年6月轉往台北空總當代文化實驗場展出。',
-        t5: '',
-        t6: '',
+    BuNam_3: {
+      Title: "荷蘭國家博物館個展",
+      t1: "這個展覽讓卜湳文明物件與阿姆斯特丹國家博物館的亞洲展館所展示的遠古文化互相對話。卜湳文明系列中的四件作品，安置於亞洲展館中的不同角落。這次展出歷時3年，展出文物的精美造形與工藝水準，也深獲好評並製造了許多話題。",
+      t2: "其中最令人視覺震撼的是大型雕塑《魂遁之舟》，這件「挖掘出土」的卜湳作品，其功能教人不得而知。靜靜站在樓梯底端落地窗旁的《魂遁之舟》，狀似飛碟下方卻有船的造型，彷彿不久前才剛飛入展館並降落在此，或即將航行到外面的水塘。步下樓梯的轉角處，可以看到一個標示為「BM0803」的物件，是一尊多頭獨身的神像雕塑，每張臉都貌似機械臉孔，所有的頭都連接到同一個結構，彷彿有多重身份的隱喻。",
+      t3: "到了地下室一樓的亞洲展館，在龍門石窟的彌勒遺跡以及天龍山石窟菩薩軀體雕塑對面的展牆上，有一件鏤空銅雕作品《繁花似錦》。這塊銅雕融合了各式不同的花卉與蝴蝶元素，也許是卜湳文明的建築遺跡。但靠近作品一看，就會發現所有花朵與蝴蝶都是由類似不明電子機械零件所組成。",
+      t4: "另一件標號「BM7005」的物件，隱身在漢代到六朝的中國古代物件之間。BM7005是一塊類似手機並銘刻文字的石碑，上面這些無法辨識的文字，根源於不同時代與文化的形狀與圖案交織，呈現出時空地域錯亂的悖謬之感。",
+      t5: "",
+      t6: "",
     },
-    BuNam_4:{
-        Title : "",
-        t1: '',
-        t2: '',
-        t3: '',
-        t4: '',
-        t5: '',
-        t6: '',
-    }
+    BuNam_4: {
+      Title: "",
+      t1: "",
+      t2: "",
+      t3: "",
+      t4: "",
+      t5: "",
+      t6: "",
+    },
+    Collection_1: {
+      Title: "巨人巨獸化石　　　　　　　　　　",
+
+      imgTitle_1: "巨人斯金納1號 Skinner No.1",
+      imgDescription_1_1: "2020 巨人斯金納1號 Skinner No.1",
+      imgDescription_1_2: "342x156xH238",
+
+      imgTitle_2: "巨獸-鯊鹿兒",
+      imgDescription_2_1: "2020 遺跡化石 - 巨獸-鯊鹿兒",
+      imgDescription_2_2: "342x156xH200",
+
+      imgTitle_3: "巨獸鳥化石1號",
+      imgDescription_3_1: "2020 遺跡化石 - 巨獸鳥化石1號",
+      imgDescription_3_2: "102x60x60",
+
+      imgTitle_4: "",
+      imgDescription_4_1: "",
+      imgDescription_4_2: "",
+
+      imgTitle_6: "鯨魚1號 (Whale Monster 01)",
+      imgDescription_6_1: "2021 遺跡化石 - 鯨魚1號 (Whale Monster 01)",
+      imgDescription_6_2: "1140x320x55(1824)	",
+    },
+    Collection_2: {
+      Title: "生物化石　　　　　　　　　　　　",
+
+      imgTitle_7: "MB 02局部01",
+      imgDescription_7_1: "2019 遺跡化石Trace Fossil-MB 02局部01",
+      imgDescription_7_2: "455x150x15(341)",
+
+      imgTitle_8: "MB 04",
+      imgDescription_8_1: "2019 遺跡化石Trace Fossil-MB 04",
+      imgDescription_8_2: "200x103x14(103)",
+
+      imgTitle_9: "MB 03",
+      imgDescription_9_1: "2019 遺跡化石Trace Fossil-MB 03",
+      imgDescription_9_2: "182x103x12(93)",
+
+      imgTitle_10: "MB 13",
+      imgDescription_10_1: "2020 遺跡化石Trace Fossil-MB 13",
+      imgDescription_10_2: "184x103x9(95)",
+
+      imgTitle_11: "MB 16",
+      imgDescription_11_1: "2020 遺跡化石Trace Fossil-MB 16",
+      imgDescription_11_2: "296x138x14(204)",
+
+      imgTitle_12: "MB 14",
+      imgDescription_12_1: "2020 遺跡化石Trace Fossil-MB 14",
+      imgDescription_12_2: "140x60x10(42)",
+
+      imgTitle_13: "MB 08",
+      imgDescription_13_1: "2020 遺跡化石Trace Fossil-MB 08",
+      imgDescription_13_2: "74x52x10(19)",
+
+      imgTitle_14: "MB 06",
+      imgDescription_14_1: "2020 遺跡化石Trace Fossil-MB 06",
+      imgDescription_14_2: "60×38×10(11.4)",
+
+      imgTitle_15: "MB 07",
+      imgDescription_15_1: "2020 遺跡化石Trace Fossil-MB 07",
+      imgDescription_15_2: "53×42.5×5(11.2)",
+
+      imgTitle_16: "MB 09",
+      imgDescription_16_1: "2020 遺跡化石Trace Fossil-MB 09",
+      imgDescription_16_2: "62x53x5.5(16.4)",
+
+      imgTitle_17: "MB 10",
+      imgDescription_17_1: "2020 遺跡化石Trace Fossil-MB 10",
+      imgDescription_17_2: "50x60.5x4(15)",
+
+      imgTitle_18: "MB 11",
+      imgDescription_18_1: "2020 遺跡化石Trace Fossil-MB 11",
+      imgDescription_18_2: "87x42.5x8(18.5)",
+
+      imgTitle_19: "MB 15",
+      imgDescription_19_1: "2022 遺跡化石Trace Fossil-MB 15",
+      imgDescription_19_2: "57x71x25(20)",
+
+      imgTitle_20: "MB 12",
+      imgDescription_20_1: "2020 遺跡化石Trace Fossil-MB 12",
+      imgDescription_20_2: "98x89x4(43.6)",
+
+      imgTitle_37: "MB 17",
+      imgDescription_37_1: "2020 遺跡化石Trace Fossil-MB 17",
+      imgDescription_37_2: "40x27x8(5.4)	",
+    },
+    Collection_3: {
+      Title: "植物化石　　　　　　　　　　　　",
+
+      imgTitle_21: "PM 01",
+      imgDescription_21_1: "2020 遺跡化石Trace Fossil-PM 01",
+      imgDescription_21_2: "310x111x6(172)",
+
+      imgTitle_22: "PM 19",
+      imgDescription_22_1: "2020 遺跡化石Trace Fossil-PM 19",
+      imgDescription_22_2: "159x120x6.5(95)",
+
+      imgTitle_23: "PM 18",
+      imgDescription_23_1: "2020 遺跡化石Trace Fossil-PM 18",
+      imgDescription_23_2: "169x83x9(70)",
+
+      imgTitle_24: "PM 16",
+      imgDescription_24_1: "2020 遺跡化石Trace Fossil-PM 16",
+      imgDescription_24_2: "160x50x7(40)",
+
+      imgTitle_25: "PM 05",
+      imgDescription_25_1: "2022 遺跡化石Trace Fossil-PM 05",
+      imgDescription_25_2: "152x60x6(45.6)",
+
+      imgTitle_26: "PM 07",
+      imgDescription_26_1: "2022 遺跡化石Trace Fossil-PM 07",
+      imgDescription_26_2: "80x39x4(15.6)",
+
+      imgTitle_27: "PM 06",
+      imgDescription_27_1: "2022 遺跡化石Trace Fossil-PM 06",
+      imgDescription_27_2: "142x36x3(25.5)",
+
+      imgTitle_28: "PM 21",
+      imgDescription_28_1: "2020 遺跡化石Trace Fossil-PM 21",
+      imgDescription_28_2: "61×28×4(8.5)",
+
+      imgTitle_29: "PM 22",
+      imgDescription_29_1: "2020 遺跡化石Trace Fossil- PM 22",
+      imgDescription_29_2: "40x52x6(10.4)",
+
+      imgTitle_30: "PM 23",
+      imgDescription_30_1: "2020 遺跡化石Trace Fossil-PM 23",
+      imgDescription_30_2: "42x29x5(6)",
+
+      imgTitle_31: "PM 25",
+      imgDescription_31_1: "2020 遺跡化石Trace Fossil-PM 25",
+      imgDescription_31_2: "47x30x6(7)",
+
+      imgTitle_32: "PM 26",
+      imgDescription_32_1: "2020 遺跡化石Trace Fossil-PM 26",
+      imgDescription_32_2: "30x51x5.5(7.65)",
+
+      imgTitle_33: "PM 27",
+      imgDescription_33_1: "2020 遺跡化石Trace Fossil-PM 27",
+      imgDescription_33_2: "49x40x4(9.8)	",
+
+      imgTitle_34: "PM 28",
+      imgDescription_34_1: "2020 遺跡化石Trace Fossil-PM 28",
+      imgDescription_34_2: "40x30x4(6)",
+
+      imgTitle_35: "PM 29",
+      imgDescription_35_1: "2022 遺跡化石Trace Fossil-PM 29",
+      imgDescription_35_2: "24.5x32x2(3.9)",
+
+      imgTitle_36: "PM 30",
+      imgDescription_36_1: "2022 遺跡化石Trace Fossil-PM 30",
+      imgDescription_36_2: "21x17x2(1.8)",
+
+    },
   },
 };
 
@@ -199,3 +358,31 @@ let zhtw = {
 //     t5: '',
 //     t6: '',
 // }
+
+// Collection_2: {
+//   Title: "　　　　　　　　　　",
+
+//   imgTitle_1 : "",
+//   imgDescription_1_1 : "",
+//   imgDescription_1_2 : "",
+
+//   imgTitle_2 : "",
+//   imgDescription_2_1 : "",
+//   imgDescription_2_2 : "",
+
+//   imgTitle_3 : "",
+//   imgDescription_3_1 : "",
+//   imgDescription_3_2 : "",
+
+//   imgTitle_4 : "",
+//   imgDescription_4_1 : "",
+//   imgDescription_4_2 : "",
+
+//   imgTitle_5 : "",
+//   imgDescription_5_1 : "",
+//   imgDescription_5_2 : "",
+
+//   imgTitle_6 : "",
+//   imgDescription_6_1 : "",
+//   imgDescription_6_2 : "",
+// },
